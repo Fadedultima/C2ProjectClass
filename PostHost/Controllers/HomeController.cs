2d7431171bf8a23c2c1c5b18dc8ce6c42668ba96
Alexander Devine <alexdevine387@gmail.com>
2016-08-05 10:26:50 -0600

    Added stuff for blob storage

--- PostHost/Controllers/HomeController.cs
+++ PostHost/Controllers/HomeController.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.Mvc;
 using PostHost.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+using System.Net;
+using System.IO;
 
 namespace PostHost.Controllers
 {
@@ -12,6 +17,28 @@ namespace PostHost.Controllers
     {
         public ActionResult Index()
         {
+            // Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("BlobConnection"));
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("imagecontainer");
+
+            // Create the container if it doesn't already exist.
+            //container.CreateIfNotExists();
+            //container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("meowblob");
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+            WebRequest req = WebRequest.Create("http://i2.kym-cdn.com/photos/images/facebook/001/070/061/d96.jpg");
+            using (Stream stream = req.GetResponse().GetResponseStream())
+            {
+                blockBlob.UploadFromStream(stream);
+            }
+
             return View();
         }
 
