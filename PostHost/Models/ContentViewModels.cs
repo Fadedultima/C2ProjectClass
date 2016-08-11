5cc8bb7af9cc85dae4a54b63883c6c1f5d92fa62
MansonZhao <genpung@yahoo.com>
2016-08-11 09:49:18 -0600

    someone is still pushing the suo file

---
+++ PostHost/Models/ContentViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostHost.Models
+{
+    public class ContentViewModels
+    {
+        public Content theContent { get; set; }
+        public List<Tag> theTags = new List<Tag>();
+
+    }
+}
